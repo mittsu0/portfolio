60866cc1a53173f6ee859acd6539dc7158a89610
mittsu0 <threebridge3824@gmail.com>
2022-06-12 14:05:36 +0900

    [add]laravelにfontawesomeのパッケージをインストール

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/test', function(){
-    return view('test');
-});
+Route::get('/',[App\Http\Controllers\ArticleController::class, 'index']);
