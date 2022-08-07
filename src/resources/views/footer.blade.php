55ad31d39f2992454dc70e179275274b8ceeaa8d
mittsu0 <threebridge3824@gmail.com>
2022-08-07 14:14:32 +0900

    [add]フッターのviewを追加

---
+++ src/resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer d-flex justify-content-center align-items-center gap-3">
+    <a href="" class="footer-link link-item py-1">本サイトについて</a>
+    <a href="{{route('contacts.create')}}" class="footer-link link-item py-1">お問い合わせ</a>
+    </div>
+</footer>
+
+{{-- <footer class="footer d-flex justify-content-center align-items-center gap-3">
+    <a href="" class="footer-link link-item py-1">本サイトについて</a>
+    <a href="{{route('contacts.create')}}" class="footer-link link-item py-1">お問い合わせ</a>
+    </div>
+</footer> --}}
