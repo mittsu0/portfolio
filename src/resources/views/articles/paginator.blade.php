304c1e5695e8a54bb7881d6078681896120efe47
mittsu0 <threebridge3824@gmail.com>
2022-08-11 20:42:48 +0900

    [update]ページネーションのviewを修正

--- src/resources/views/articles/paginator.blade.php
+++ src/resources/views/articles/paginator.blade.php
@@ -1,20 +1,24 @@
 @if ($paginator->hasPages())
     @php
+        //link表示数を入力してくだい
+        $link_count= 7;
+        $middle_each_link_count= 1 + ($link_count - 7) / 2;
+        $side_link_count= $link_count-2;
         $first_page= 1;
         $last_page= $paginator->lastPage();
         $current_page= $paginator->currentPage();
     @endphp
         <nav>
             <ul class="pagination">
-                @if($last_page <= 7)
+                @if($last_page <= $link_count)
                     @for($i = $first_page; $i <= $last_page; $i++)
                         <li class="page-item @if($i === $current_page) active @endif">
                             <a href="{{ $paginator->url($i) }}" class="page-link">{{ $i }}</a>
                         </li>
                     @endfor
                 @else
-                    @if($current_page <= $first_page + 3)
-                        @for($i = $first_page; $i <= $first_page + 4; $i++)
+                    @if($current_page < $side_link_count)
+                        @for($i = $first_page; $i <= $side_link_count; $i++)
                             <li class="page-item @if($i === $current_page) active @endif">
                                 <a href="{{ $paginator->url($i) }}" class="page-link">{{ $i }}</a>
                             </li>
@@ -25,14 +29,14 @@
                         <li class="page-item">
                             <a href="{{ $paginator->url($last_page) }}" class="page-link">{{ $last_page }}</a>
                         </li>
-                    @elseif($current_page >= $last_page - 3)
+                    @elseif($current_page > $last_page - $side_link_count +1)
                         <li class="page-item">
                             <a href="{{ $paginator->url($first_page) }}" class="page-link">{{ $first_page }}</a>
                         </li>
                         <li aria-disabled="true" class= "page-item disabled">
                             <span class="page-link">...</span>
                         </li>
-                        @for($i = $last_page - 4; $i <= $last_page; $i++)
+                        @for($i = $last_page - $side_link_count + 1; $i <= $last_page; $i++)
                             <li class="page-item @if($i === $current_page) active @endif">
                                 <a href="{{ $paginator->url($i) }}" class="page-link">{{ $i }}</a>
                             </li>
@@ -44,7 +48,7 @@
                         <li aria-disabled="true" class= "page-item disabled">
                             <span class="page-link">...</span>
                         </li>
-                        @for($i = $current_page-1; $i <= $current_page+1; $i++)
+                        @for($i = $current_page-$middle_each_link_count; $i <= $current_page+$middle_each_link_count; $i++)
                             <li class="page-item @if($i === $current_page) active @endif">
                                 <a href="{{ $paginator->url($i) }}" class="page-link">{{ $i }}</a>
                             </li>
