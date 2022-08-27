cca0a74af4dde36824ba437d3c0b5d0f7ceed67a
mittsu0 <threebridge3824@gmail.com>
2022-08-27 20:30:06 +0900

    [hotfix]dockerの設定ファイルを修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Article::factory()->count(100)->create();
-        \App\Models\Comment::factory()->count(300)->create();
-        \App\Models\Good::factory()->count(400)->create();
+        \App\Models\Article::factory()->count(300)->create();
+        \App\Models\Comment::factory()->count(600)->create();
+        \App\Models\Good::factory()->count(1000)->create();
     }
 }
