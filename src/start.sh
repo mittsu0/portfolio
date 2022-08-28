afee3d215f4dd883049b0682703ae32f622dc0ce
mittsu0 <threebridge3824@gmail.com>
2022-08-28 18:34:49 +0900

    [update]本番用にdockerfileを作成

--- src/start.sh
+++ src/start.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+composer install --optimize-autoloader --no-dev
 chmod -R 777 storage/* bootstrap/cache
-php artisan config:cache
-php artisan view:cache
-php artisan route:cache
+php artisan optimize:clear
+php artisan optimize
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 php artisan migrate --force
 php artisan storage:link
-composer install --optimize-autoloader --no-dev
-npm install
+npm ci
 npm run prod
