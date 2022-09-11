2c6e8306b37c127bf839b68c73f4ddbe9a60010b
mittsu0 <threebridge3824@gmail.com>
2022-09-11 10:33:10 +0900

    [add]スニペットを追加

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name=”description” content=”ONEDARI-oオネダリ-は、みんなの「ホシイ」を共有するためのサービスです。ログインなし・匿名で気軽にご利用いただけます。”>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
